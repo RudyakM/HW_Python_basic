496e91917359aae4d9d435143873c1b1c0cc2e82
RudyakM <rudyakmaxl@gmail.com>
2023-11-15 15:53:27 +0200

    add hw 14

---
+++ Tasks/hw14/29.py
@@ -0,0 +1,26 @@
+numbers = {
+    '+3': '0634167093',
+    '+6': '0931010715',
+    '+5': '0732990061',
+    '+12': '0482526694',
+    '+4': '0635167090',
+    '+7': '0636167098',
+}
+result = []
+# 1
+print('# 1')
+print([numbers[i] for i in sorted(numbers, key= lambda x: int(x))])
+
+
+# 2
+print('# 2')
+print([i for i in sorted(numbers.values(), key= lambda x: int(x), reverse=True)])
+
+
+# 3
+print('# 3')
+print([i for i in sorted(numbers.values(), key= lambda x: x[3:])])
+
+# 4
+print('# 4')
+print([i for i in sorted(numbers.values(), key= lambda x: x[-1])])
