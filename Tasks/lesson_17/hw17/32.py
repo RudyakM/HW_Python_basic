6614d3ce1b882dcb45aa8148597b26e2b9f1d5cb
RudyakM <rudyakmaxl@gmail.com>
2023-11-18 14:13:42 +0200

    change

--- Tasks/lesson_17/hw17/32.py
+++ Tasks/lesson_17/hw17/32.py
@@ -4,23 +4,23 @@ history = ''
 
 if __name__ == '__main__':
     while True:
-        operand_1 = int(input('Число 1: '))
-        operand_2 = int(input('Число 2: '))
-        operation = input('Введите операцию (+, -, *, /, //, %): ')
+        operand_1 = int(input('Num 1: '))
+        operand_2 = int(input('Num 2: '))
+        operation = input('Operation: (+, -, *, /, //, %): ')
         
         if operation == '//' and (operand_1 or operand_2) == 0:
-            print('На ноль делить незьзя...') 
+            print('Divided by zero') 
             continue
         elif operation not in ('+', '-', '*', '/', '%', '//'): 
-            print('Такой операции нет...')
+            print('Такой операции нет')
             continue
         else:
             result = calc(operand_1, operation, operand_2)
             history += f'{operand_1} {operation} {operand_2} = {result}\n'
             print(f'{operand_1} {operation} {operand_2} = {result}')
         
-        if input('Показать историю y/n: ') == 'y':
+        if input('History? y/n: ') == 'y':
             print(f'\nHistory:\n{history}')
-        if input('Хотите продолжить y/n: ') == 'n':
+        if input('Continue? y/n: ') == 'n':
             print('Bye')
             break
