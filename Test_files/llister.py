9c0dcb15ba2c055da3efe4f41f7c2d0832e08874
RudyakM <rudyakmaxl@gmail.com>
2023-11-18 13:21:27 +0200

    change folder name

---
+++ Test_files/llister.py
@@ -0,0 +1,4 @@
+strk = 'Hello!'
+lst = list(strk)
+print(lst[len(lst) - 1])
+print(lst)
---
+++ Test_files/lst_dir.py
@@ -0,0 +1,10 @@
+# lst = []
+# print(dir(lst))
+# mnoj = set()
+# print(dir(mnoj))
+svr =  {'sport': {'football', 'basketball', 'voleyball'},
+        'fruit': {'apple', 'banana', 'orange'},
+        'colours': {'red', 'yellow', 'green'}
+        }
+print(dir(svr))
+print(svr.keys())
