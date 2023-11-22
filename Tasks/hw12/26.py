94bc964af71044f08fbbc6ab561dff01211bb86f
RudyakMaksym <rudyakmaxl@gmail.com>
2023-11-22 10:44:33 +0200

    fully written program, bug fixes

--- Tasks/hw12/26.py
+++ Tasks/hw12/26.py
@@ -1,18 +1,28 @@
-lst = [
+def round_cost(x):
+    total_cost = list()
+    for i in cost_order:
+        i = round(i, 2)
+        total_cost.append(i)
+    order_cost = zip(order, total_cost)
+    print(list(order_cost))
+    return order_cost
+    
+
+contacts = [
     [34587, 'Learning Python, Mark Lutz', 4, 40.95],
     [98762, 'Programming Python, Mark Lutz', 5, 56.80],
     [77226, 'Head First Python, Paul Barry', 3, 32.95],
     [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]
     ]
-result = []
 
-for item in lst:
-    for i in item:
-        if type(i) == str:
-            item.remove(i)
-    
-    if item[-1] * item[-2] < 100:
-       item[-1] += 10
-    result.append(item)
+order = list()
+amount = list()
+price = list()
 
-print(list(map(lambda x: tuple(x), result)))
+for i in contacts:
+    order.append(i[0])
+    amount.append(i[2])
+    price.append(i[3])
+
+cost_order = list(map(lambda x, y: x * y if x * y > 100 else x * y + 10, price, amount))
+round_cost(x=cost_order)
