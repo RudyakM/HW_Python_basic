d6200820a213110bdb370bb8fb3f1061dff4919e
RudyakMaksym <rudyakmaxl@gmail.com>
2023-11-22 10:00:17 +0200

    better list visibility

--- Tasks/hw12/26.py
+++ Tasks/hw12/26.py
@@ -1,7 +1,9 @@
-lst = [[34587, 'Learning Python, Mark Lutz', 4, 40.95],
-            [98762, 'Programming Python, Mark Lutz', 5, 56.80],
-            [77226, 'Head First Python, Paul Barry', 3, 32.95],
-            [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]]
+lst = [
+    [34587, 'Learning Python, Mark Lutz', 4, 40.95],
+    [98762, 'Programming Python, Mark Lutz', 5, 56.80],
+    [77226, 'Head First Python, Paul Barry', 3, 32.95],
+    [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]
+    ]
 result = []
 
 for item in lst:
@@ -13,6 +15,4 @@ for item in lst:
        item[-1] += 10
     result.append(item)
 
-
-result = list(map(lambda x: tuple(x), result))
-print(result)
+print(list(map(lambda x: tuple(x), result)))
