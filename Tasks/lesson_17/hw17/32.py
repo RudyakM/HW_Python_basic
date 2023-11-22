4b0043e7b5b665ef88141559694a5d8f3db9f9bc
RudyakMaksym <rudyakmaxl@gmail.com>
2023-11-22 11:42:30 +0200

    update func calc

--- Tasks/lesson_17/hw17/32.py
+++ Tasks/lesson_17/hw17/32.py
@@ -3,21 +3,21 @@ from module import Calcilation
 history = ''
 if __name__ == '__main__':
     while True:
-        operand_1 = int(input('Num 1: '))
-        operand_2 = int(input('Num 2: '))
+        item_1 = int(input('Num 1: '))
+        item_2 = int(input('Num 2: '))
         operation = input('Operation: (+, -, *, /, //, %): ')
         
-        if operation == '//' and (operand_1 or operand_2) == 0:
+        if operation == '//' and (item_1 or item_2) == 0:
             print('Divided by zero') 
             continue
         elif operation not in ('+', '-', '*', '/', '%', '//'): 
             print('Такой операции нет')
             continue
         else:
-            pointer = Calcilation(operand_1, operation, operand_2, history)
-            result = pointer.calc()
+            pointer = Calcilation(item_1, operation, item_2, history)
+            result = pointer.calc()(item_1, item_2)
             history = pointer.historic(result)
-            print(f'\n{operand_1} {operation} {operand_2} = {result}\n')
+            print(f'\n{item_1} {operation} {item_2} = {result}\n')
         
         if input('History? y/n: ') == 'y':
             pointer.print_history()
--- Tasks/lesson_17/hw17/module.py
+++ Tasks/lesson_17/hw17/module.py
@@ -1,36 +1,27 @@
 class Calcilation:
 
-    def __init__(self, operand_1=0, operation='+', operand_2=0, history=''):
-        self.operand_1 = operand_1
-        self.operand_2 = operand_2
+    def __init__(self, item_1=0, operation='+', item_2=0, history=''):
+        self.item_1 = item_1
+        self.item_2 = item_2
         self.operation = operation
         self.history = history
 
     def calc(self):
         operation = self.operation
-        operand_1 = self.operand_1
-        operand_2 = self.operand_2
-        if operation == '+':
-            result = operand_1 + operand_2
-            return result 
-        elif operation == '-':
-            result = operand_1 - operand_2 
-            return result 
-        elif operation == '*':
-            result = operand_1 * operand_2
-            return result 
-        elif operation == '/':
-            result = operand_1 / operand_2 
-            return result 
-        elif operation == '%':
-            result = operand_1 % operand_2
-            return result 
-        else:
-            result = operand_1 // operand_2 
-            return result
+        
+        d = {'+': lambda x, y: x + y,
+             '-': lambda x, y: x - y,
+             '*': lambda x, y: x * y,
+             '/': lambda x, y: x * y,
+             '//': lambda x, y: x * y,
+             '/': lambda x, y: x * y,
+            }
+        
+        return d.get(operation)
+
         
     def historic(self, result):
-        self.history += f'{self.operand_1} {self.operation} {self.operand_2} = {result}\n'
+        self.history += f'{self.item_1} {self.operation} {self.item_2} = {result}\n'
         return self.history
 
     def print_history(self):
